89d93f205dc84102e0c7011201f4af7f6aa464c3
jawanb <91311984+jawanb@users.noreply.github.com>
2022-02-08 17:42:41 +0100

    Dobbelstenen

--- les01Syntax/Dobbelstenen/js/scripts.js
+++ les01Syntax/Dobbelstenen/js/scripts.js
@@ -2,13 +2,13 @@
 console.log(Math.ceil(Math.random() * 10));
 
 let players = [];
-players [0] = 'Indra';
-players [1] = 'Nelly';
-players [2] = 'Ron';
-players [3] = 'Prasha';
-players [4] = 'Zakaria';
+players[0] = 'Indra';
+players[1] = 'Nelly';
+players[2] = 'Ron';
+players[3] = 'Prasha';
+players[4] = 'Zakaria';
 
-let scores = [players.length];
+let scores = [];
 const numDice = 3;
 
 // show header
@@ -17,13 +17,19 @@ console.log(`DOBBELSTENEN
 aantal deelnemers: ${players.lenght}`);
 
 // throw dice
-for (let i = 0; i < players.length; i++) 
-    {Console.log(`${players[i]} gooit: `);
-    let dice = [NUM_DICE];
+for (let i = 0; i < players.length; i++) {
+    console.log(`${players[i]} gooit: `);
+    let dice = [];
     for (let j = 0; j < NUM_DICE; j++) {
-    dice[j] = rnd.Next(1, 7);
+        dice[j] = rnd.Next(1, 7);
     }
     scores[i] = getTotal(dice);
-    Console.log(`${diceToString(dice)} (${scores[i]} punten)`);
-    }
-    
+    console.log(`${diceToString(dice)} (${scores[i]} punten)`);
+
+    // show winner
+    Console.log(`${Environment.NewLine}WINNAAR: `);
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.log(`${getWinner(scores, players)}`);
+    Console.ReadLine();
+}
